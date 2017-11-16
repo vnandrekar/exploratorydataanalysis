4c8c8e55164ea9d25cda8063074600f507b7d16f
vnandrekar <vikrant.nandrekar@gmail.com>
2017-11-16 15:53:51 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -13,3 +13,7 @@ with(data2, {
   lines(Sub_metering_3~Datetime,col='Blue')
 })
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+##PNG file
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()

fae7b6e2577e4f39d173e232cff7fd3de82f0a0c
vnandrekar <vikrant.nandrekar@gmail.com>
2017-11-16 14:41:37 +0100

    Update Plot1.r

--- Plot1.r
+++ Plot1.r
@@ -3,3 +3,7 @@ setwd("C:/Users/vnandrekar/Documents/data")
 data_all <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data2 <- subset(data_full, Date %in% c("16/12/2006","18/12/2006"))
  hist(data2$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="blue")
+
+
+png("plot1.png", width=480, height=480)
+dev.off()

b64172d9164f58f0bcec22e7b7335e7c97ecb3d9
vnandrekar <vikrant.nandrekar@gmail.com>
2017-11-16 18:02:11 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -2,7 +2,7 @@
 getwd()
 setwd("C:/Users/vnandrekar/Documents/data")
 data_all <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data2 <- subset(data_full, Date %in% c("1/2/2007","2/2/2007"))
+data2 <- subset(data_all, Date %in% c("1/2/2007","2/2/2007"))
 data2$Date <- as.Date(data2$Date, format="%d/%m/%Y")
 datetime <- paste(as.Date(data2$Date), data2$Time)
 data2$Datetime <- as.POSIXct(datetime)

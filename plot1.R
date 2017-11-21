34bded4a3a2f286057f6c41e89eec05503c5792f
vnandrekar <vikrant.nandrekar@gmail.com>
2017-11-21 18:47:39 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -7,5 +7,5 @@ hist(data2$Global_active_power, main="Global Active Power", xlab="Global Active 
 
 
 ##export to PNG
-png("plot1.png", width=480, height=480)
+dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
